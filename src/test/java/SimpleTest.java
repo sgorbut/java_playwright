6a69ebabca6b1d89c78eeaca5d468a1bbe3a0c45
Sergey Gorbut <gorbut_sa@magnit.ru>
2023-03-17 22:54:11 +0300

    Added click by locator

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,19 +1,26 @@
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 public class SimpleTest {
 
     @Test
-    public void firstTest(){
+    public void firstTest() throws InterruptedException {
 
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        Page page = browser.newPage();
+        BrowserContext browserContext = browser.newContext(new Browser.NewContextOptions().setPermissions(Arrays.asList("geolocation")));
+        Page page = browserContext.newPage();
 
-        page.navigate("https:google.com");
+        page.navigate("https://www.bestbuy.com/?intl=nosplash");
+//        page.locator("//a[@data-lid='hdr_dotd']").click();    //xpath
+//        page.locator("a:has-text('Deal of the Day')").click();  //text
+
+        Locator dealOfTheDay = page.locator("a:has-text('Deal of the Day')");
+        dealOfTheDay.click();
+
+        Thread.sleep(5000);
 
     }
 
