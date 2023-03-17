9c5ac9c4a7814711482c0d3151fb690f4bdf246d
Sergey Gorbut <gorbut_sa@magnit.ru>
2023-03-17 21:12:06 +0300

    Added simple test

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,20 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void firstTest(){
+
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+
+        page.navigate("https:google.com");
+
+    }
+
+}
